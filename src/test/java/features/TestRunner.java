0bd94a6df2924553d7fee68229035307b3f9f0e1
Curry Pham <khoapd2000@gmail.com>
2023-06-12 05:49:43 +0700

    Test Runner in Karate Junit5 || Run Feature Files from Maven Command Line

---
+++ src/test/java/features/TestRunner.java
@@ -0,0 +1,26 @@
+//package features;
+//
+//import com.intuit.karate.junit5.Karate;
+//
+//public class TestRunner{
+//
+//    @Karate.Test
+//    public void getApiTest(){
+//        return Karate.run("GetAPI").relativeTo(getClass());
+//    }
+//    @Karate.Test
+//    public void postApiTest(){
+//        return Karate.run("PostAPI").relativeTo(getClass());
+//    }
+//    @Karate.Test
+//    public void putApiTest(){
+//        return Karate.run("PutAPI").relativeTo(getClass());
+//    }
+//    @Karate.Test
+//    public void deleteApiTest(){
+//        return Karate.run("DeleteAPI").relativeTo(getClass());
+//    }    @Karate.Test
+//    public void jsonTest(){
+//        return Karate.run("json").relativeTo(getClass());
+//    }
+//}
